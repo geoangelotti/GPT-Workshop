6bce481624250f5a518d1fa8efc6574cbe8f395a
Alfred Feldmeyer <alfred.feldmeyer@squer.io>
2025-05-21 03:00:49 +0200

    adjusted installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+conda create --name WORKSHOP_GPT -c conda-forge python=3.11 -y
+conda activate WORKSHOP_GPT
+export PATH=$HOME/.local/bin:$PATH
 curl -sSL https://pdm-project.org/install-pdm.py | python3 -
-pdm config python.use_venv false
-pdm install
+cd $HOME/SageMaker/GPT-Workshop
+pdm install -q
+
+python3 -m ipykernel install --user --name WORKSHOP_GPT --display-name "WORKSHOP-GPT"
