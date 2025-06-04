230e89a8f6eb7de9f9e66cb9e611772dac803df0
Alfred Feldmeyer <alfred.feldmeyer@squer.io>
2025-06-04 19:11:38 +0200

    unified deps and installation

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conda create --name WORKSHOP_GPT -c conda-forge python=3.11 -y
-conda activate WORKSHOP_GPT
-export PATH=$HOME/.local/bin:$PATH
-curl -sSL https://pdm-project.org/install-pdm.py | python3 -
-cd $HOME/SageMaker/GPT-Workshop
-pdm install -q
-
-.venv/bin/python -m ipykernel install --user --name WORKSHOP_GPT --display-name "WORKSHOP-GPT"
